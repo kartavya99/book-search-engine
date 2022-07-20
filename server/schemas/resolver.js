374dfaebebfe2f410da0d8394f845f67ae376a51
kartavya <kartavyas@gmail.com>
2022-07-20 19:52:39 +1000

    debug the error but still sign up not working

--- server/schemas/resolver.js
+++ server/schemas/resolver.js
@@ -7,15 +7,15 @@ const resolvers = {
     // By adding context to our query, we can retrieve the logged in user without specifically searching for them
     me: async (parent, args, context) => {
       if (context.user) {
-        return User.findOne({ _id: context.user._id });
+        return User.findOne({ _id: context.user._id }).select("_V -password");
       }
       throw new AuthenticationError("You need to be logged in!");
     },
   },
 
   Mutation: {
-    addUser: async (parent, { name, email, password }) => {
-      const user = await User.create({ name, email, password });
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
       const token = signToken(user);
 
       return { token, user };
