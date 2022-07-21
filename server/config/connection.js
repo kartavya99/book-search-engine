64a62041cf091fddbbc656ea972f85540448630b
kartavya <kartavyas@gmail.com>
2022-07-21 17:34:45 +1000

    updated config

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/google-books",
+  process.env.MONGODB_URI || "mongodb://localhost/google-books",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
